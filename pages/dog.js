3b662155549fe9576c1965143f42381a636a77cb
pavan kumar kulkarni <pavankumar.kulkarni@gmail.com>
2022-05-08 16:43:29 -0400

    zdfg

---
+++ pages/dog.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import Link from "next/link"
+import Image from "next/image"
+export default function DogImage() {
+  const [dog, setDog] = useState(null);
+  useEffect(() => {
+    async function getDog() {
+      const res = await fetch("https://dog.ceo/api/breeds/image/random");
+      const data = await res.json();
+      setDog(data.message);
+    }
+    getDog();
+  }, []);
+
+  return (<>
+  <Image src={dog} alt="cute pet" style={{height:'200px',display:'block'}}/>
+  <Link href="/"><a>Home</a></Link>
+  </>)
+};
+
